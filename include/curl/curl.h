1d9ec49f5ecef2faca9f09a464b9d4c92ff259a6
Jay Satiro <raysatiro@yahoo.com>
2015-10-01 14:08:46 -0400

    CURLINFO_TLS_SSL_PTR: Proposed option to get active SSL object. draft3
    fix CURLINFO_LASTONE setting

--- include/curl/curl.h
+++ include/curl/curl.h
@@ -2144,7 +2144,7 @@ typedef enum {
   CURLINFO_TLS_SSL_PTR      = CURLINFO_STRING + 45,
   /* Fill in new entries below here! */
 
-  CURLINFO_LASTONE          = 44
+  CURLINFO_LASTONE          = 45
 } CURLINFO;
 
 /* CURLINFO_RESPONSE_CODE is the new name for the option previously known as
